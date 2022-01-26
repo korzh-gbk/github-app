e44bffe10025df8329383f8dd9a66db8841a08ee
Artem Korzh <korzh.aa@gbksoft.com>
2022-01-26 17:29:49 +0200

    Initial Commit

---
+++ Github App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Github App
+//
+//  Created by Artem Korzh on 26.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
